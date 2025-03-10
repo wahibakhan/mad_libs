7e9775a0bb07caf76e20b031f0e96d2af84927e1
wahibakhan <wahibak97@gmail.com>
2025-03-10 22:01:00 +0500

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,12 @@
+def madlib():
+    animal = input("Animal :")
+    color = input("Color :")
+    place = input("Place :")
+    food = input("Food :")
+    verb = input("Verb :")
+    story = f"Once upon a time, a {color} {animal} went to the {place}. It was very hungry, so it ate some {food}. Then, it decided to {verb} all day long and had the best time ever!"
+    print(story)
+
+
+madlib()
+
